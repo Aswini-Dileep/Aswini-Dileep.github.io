4e88a692a1051220450391d696e35cf308abf0bf
Aswini-Dileep <aswinidileep91@gmail.com>
2026-06-08 12:16:50 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+const form = document.getElementById("contact-form");
 
+if (form) {
+  form.addEventListener("submit", function () {
+    setTimeout(() => {
+      form.reset();
+    }, 500);
+  });
+}
